0ef7c305832727699375cdf165e8b974f91e756b
Ryan Peach <rgpeach10@gmail.com>
2013-07-01 18:56:00 -0700

    Just making sure the branch is up to date

--- CircuitGraph.py
+++ CircuitGraph.py
@@ -5,10 +5,11 @@ class CircuitGraph:
     COMPLIST = ['IMP', 'RES', 'IND', 'CAP', 'VSC', 'CSC']
     COMMLIST = ['VOL','CUR']
     
-    def __init__(self,w=0):
+    def __init__(self,STATE='DCSS',w=0):
         self.NodeList = [Node('GND',0)]
-        self.ComponentList = []
+        self.ComponentList = {}
         self.Names = {}
+        self.STATE = STATE
         self.w = w
 
     #Adds all nodes between the last node on list to node number n
@@ -16,7 +17,7 @@ class CircuitGraph:
     def addNodes(self, n):
         for i in range(len(self.NodeList),n+1):
             #Create Name
-            Name = 'V'+str(i)
+            Name = 'N'+str(i)
 
             #Check for DuplicateName Exception
             if Name in self.Names:
@@ -25,9 +26,10 @@ class CircuitGraph:
             #Add Node
             newNode = Node(Name)
             self.NodeList.append(newNode)
-            self.Names[newNode.Name]=newNode
-    
-    def addComponent(self, t, Name, Node1, Node2, Value=None):
+            self.Names[newNode.Name]=newNode.Value
+
+    #Done, not checked
+    def addComponent(self, t, Name, Node1, Node2, Value=None, Init = 0):
         #Create Nodes if they don't already exist
         if Node1 >= len(self.NodeList):
             self.addNodes(Node1)
@@ -55,19 +57,60 @@ class CircuitGraph:
         else:
             raise IncorrectLabel('This item is not a Component. Check that CircuitGraph has it listed.')                       #Just in case the programmer uses this wrong
 
-        self.ComponentList.append(newComponent)
-        self.Names[newComponent.Name]=newComponent
+        #Add to Graph
+        self.ComponentList[newComponent.Name]=newComponent
+        self.Names[newComponent.Name]=newComponent.Value
 
-#Here are the main super classes
+    def exeCommand():
+        pass
+
+    def NodalAnalysis(Circuit):
+        Unknowns = []
+        Equations = []
+
+        #Make a list of all Nodal Equations and Names
+        for n in self.NodeList:
+            self.Equations.append(self._KCL(n))
+            if n._Value == None:
+                self.Unknowns.append(n.Name)
+        for n, c in self.ComponentList.items():
+            if c._Value == None:
+                self.Unknowns.append(n)
+
+        #Simplify the Equations List
+        pass
+
+        #Solve the Circuit
+        sol = solve(Equations, Unknowns, manual=True, dict=True)[0]
+
+        #Simplify the Answers
+        Solutions = {}
+        for N, V in sol.items():
+            Solutions[N]=simplify(V)
+
+        return Equations, Unknowns, Solutions
+
+    def _KCL(NodeA, ignore=None):
+        Equ = 0
+        #For this node, add up all the currents going to each component
+        for n, c in NodeA.ComponentList.items():
+            if c.Node2 is NodeA:        #Assume current in
+                Equ += c.Current
+            elif c.Node1 is NodeA:
+                Equ -= c.Current
+
+        return Equ
+
+#Here is the main Node class
 class Node:
     def __init__(self, Name, Voltage=None):
         self.Name = Symbol(Name)
-        self.ComponentList = []
+        self.ComponentList = {}
         self._Value=Voltage
     def __str__(self):
         return str(self.Name)
     def add(self, Component):
-        self.ComponentList.append(Component)
+        self.ComponentList[Component.Name]=Component
     def set_Value(self, Value): self._Value = Value
     def get_Value(self):
         if self._Value == None:
--- Components.py
+++ Components.py
@@ -1,21 +1,18 @@
 from sympy import Symbol
 from sympy import oo
 
-#This is a function frequently used by closed circuits
-def cincout(me,Node1, Node2):
+def cincout(me):
     total = 0
-    for c in Node1.ComponentList:
+    for c in me.Node1.ComponentList:
         if c is not me:
-            if Node1 is c.Node2:
+            if me.Node1 is c.Node2:           #Convert to Passive
                 total += c.Current
-            elif Node1 is c.Node1:
+            elif me.Node1 is c.Node1:
                 total -= c.Current
 
     return total
 
-#This is the class that all Components are based on
-class Component:
-    #Main defining classes
+class Component:    
     def __init__(self, Name, Node1, Node2, Value=None, w=0):
         self.Node1=Node1
         self.Node2=Node2
@@ -23,7 +20,7 @@ class Component:
         Node2.add(self)
         self.w = w
         self.Name=Symbol(Name)
-        self.Value = Value #Not self._Value because some subclasses will modify this value
+        self.Value = Value
     def __str__(self): return str(self.Name)
 
     #Value will be used for the main attribute of the class
@@ -47,24 +44,29 @@ class Component:
 
 #Here are all the Impedance Components
 class Impedance(Component):
-    pass
+    def __init__(self, Name, Node1, Node2, Value=None, w=0):
+        self.Impedance = Value
+        Component.__init__(self, Name, Node1, Node2, Value, w)
 class Resistor(Impedance):
     pass
 class Inductor(Impedance):
     def set_Value(self,ind):
         if self.w == 0:
             self._Value = 0
+            self.Node1.Value = self.Node2.Value
         else:
             self._Value = complex(0,self.w*ind)
             
     def get_Current(self):
         if self.w == 0:            
-            return cincout(self,self.Node1,self.Node2)
+            #Current in = Current out
+            return cincout(self)
+            
         else:
             return self.Voltage/self.Value             #Assume Passive
                     
-    Current = property(get_Current)
     Value = property(Component.get_Value, set_Value)
+    Current = property(get_Current)
     
 class Capacitor(Impedance):
     def set_Value(self,c):
@@ -76,15 +78,14 @@ class Capacitor(Impedance):
         if self.w == 0:
             return 0
         else:
-            return (self.Node1.Value-self.Node2.Value)
+            return self.Voltage/self.Value
 
-    Current = property(get_Current)
     Value = property(Component.get_Value, set_Value)
+    Current = property(get_Current)
 
 #Here are all the Source Components
 class Source(Component):
     pass
-    
 class VSource(Source):
     def set_Value(self, Voltage):
         self._Value=Voltage
@@ -92,7 +93,7 @@ class VSource(Source):
     def get_Voltage(self):
         return self.Value
     def get_Current(self):
-        return cincout(self,self.Node1,self.Node2)
+        return cincout(self)
 
     Value = property(Component.get_Value,set_Value)
     Voltage = property(get_Voltage,set_Value)
