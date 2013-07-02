33f4dbb9fe79a6b2e3a527c772701b1afa20e1fe
Ryan Peach <rgpeach10@gmail.com>
2013-07-01 18:33:01 -0700

    Working DCSS, ACSS. Version 0.3

--- Components.py
+++ Components.py
@@ -1,19 +1,10 @@
 from sympy import Symbol
 from sympy import oo
 
-def cincout(me):
-    total = 0
-    for c in me.Node1.ComponentList:
-        if c is not me:
-            if me.Node1 is c.Node2:           #Convert to Passive
-                total += c.Current
-            elif me.Node1 is c.Node1:
-                total -= c.Current
-
-    return total
-
 class Component:    
     def __init__(self, Name, Node1, Node2, Value=None, w=0):
+        self._Voltage = None
+        self._Current = None
         self.Node1=Node1
         self.Node2=Node2
         Node1.add(self)
@@ -33,9 +24,15 @@ class Component:
 
     #Current and Voltage are by default derived, but will later be defined as values in other subclasses
     def get_Current(self):
-        return self.Voltage/self.Value             #Assume Passive
+        if self._Current == None:
+            return self.Voltage/self.Value             #Assume Passive
+        else:
+            return self._Current
     def get_Voltage(self):
-        return (self.Node1.Value-self.Node2.Value) #Assume Passive
+        if self._Voltage == None:
+            return (self.Node1.Value-self.Node2.Value) #Assume Passive
+        else:
+            return self._Voltage
         
 
     Value = property(get_Value, set_Value)
@@ -44,62 +41,70 @@ class Component:
 
 #Here are all the Impedance Components
 class Impedance(Component):
-    def __init__(self, Name, Node1, Node2, Value=None, w=0):
-        self.Impedance = Value
-        Component.__init__(self, Name, Node1, Node2, Value, w)
+    pass
 class Resistor(Impedance):
     pass
 class Inductor(Impedance):
     def set_Value(self,ind):
         if self.w == 0:
             self._Value = 0
-            self.Node1.Value = self.Node2.Value
         else:
             self._Value = complex(0,self.w*ind)
             
     def get_Current(self):
         if self.w == 0:            
             #Current in = Current out
-            return cincout(self)
+            self._Current = 0
+            for c in self.Node1.ComponentList:
+                if c is not self:
+                    if self.Node1 is c.Node2:
+                        self._Current += c.Current
+                    elif self.Node1 is c.Node1:
+                        self._Current -= c.Current
+
+            return self._Current
             
-        else:
+        elif self._Current == None:
             return self.Voltage/self.Value             #Assume Passive
+        else:
+            return self._Current
                     
-    Value = property(Component.get_Value, set_Value)
     Current = property(get_Current)
+    Value = property(Component.get_Value, set_Value)
     
 class Capacitor(Impedance):
     def set_Value(self,c):
         if self.w == 0:
             self._Value = complex(0,-oo)
+            self._Current = 0
         else:
             self._Value = complex(0,-1/(self.w*c))
-    def get_Current(self):
-        if self.w == 0:
-            return 0
-        else:
-            return self.Voltage/self.Value
 
     Value = property(Component.get_Value, set_Value)
-    Current = property(get_Current)
 
 #Here are all the Source Components
-class Source(Component):
-    pass
-class VSource(Source):
+class VSource(Component):
     def set_Value(self, Voltage):
         self._Value=Voltage
         self.Node1.Value = self.Node2.Value+Voltage
     def get_Voltage(self):
         return self.Value
     def get_Current(self):
-        return cincout(self)
+        total = 0
+        for c in self.Node1.ComponentList:
+            if c is not self:
+                if self.Node1 is c.Node2:           #Convert to Passive
+                    total += c.Current
+                elif self.Node1 is c.Node1:
+                    total -= c.Current
+
+        return total
 
     Value = property(Component.get_Value,set_Value)
     Voltage = property(get_Voltage,set_Value)
     Current = property(get_Current)
 
-class CSource(Source):
+class CSource(Component):
     def get_Current(self):
         if self.Value==None:
             return self.Name
